eb8b395460be1614ba7eb51b763f34fb6e96d8c5
Kissyla <kissylamottapereira@gmail.com>
2022-12-05 14:35:37 -0300

    exercicio fixação inserção de dados

--- EntradaDados/Program.cs
+++ EntradaDados/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EntadaDados
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Entre com seu nome completo");
+      string fullName = Console.ReadLine();
+
+      Console.WriteLine("Quantos quartos tem na sua casa");
+      int rooms = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Entre com o preço de um produto:");
+      string productPrice = Console.ReadLine();
+
+      Console.WriteLine($"Você digitou: {fullName}, {rooms}, {productPrice}");
+
+      Console.WriteLine("Entre com seu último nome, idade e altura (na mesma linha):");
+      string[] infos = Console.ReadLine().Split(' ');
+
+      string lastName = infos[0];
+      int age = int.Parse(infos[1]);
+      float height = float.Parse(infos[2]);
+
+      Console.WriteLine($"Você digitou: {lastName}, {age}, {height}");
+     
+    }
+  }
+}
---
+++ EntradaDados/readme.md
@@ -0,0 +1,13 @@
+Fazer um programa para executa a seguinte interação com o usuário, lendo os valores destacadoe e depois mostrar os dados na tela
+
+Entre com seu nome completo:
+**Alex Green**
+
+Quantos quartos tem na sua casa?
+**3**
+
+Entre com o preço de um produto: (saída com 2 casas decimais)
+**500.55**
+
+Entre com seu último nome, idade e altura (na mesma linha):
+**Green 27 1.73**
