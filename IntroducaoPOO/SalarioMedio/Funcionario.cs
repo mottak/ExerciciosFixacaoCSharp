972d65a97ea61338cd4558bf1658c2cd3c26599a
Kissyla <kissylamottapereira@gmail.com>
2022-12-05 16:02:01 -0300

    Exercicio Fixação Salario médio

---
+++ IntroducaoPOO/SalarioMedio/Funcionario.cs
@@ -0,0 +1,4 @@
+class Funcionario {
+  public string nome;
+  public double salario;
+}
---
+++ IntroducaoPOO/SalarioMedio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SalarioMedio
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Funcionario func1, func2;
+      func1 = new Funcionario();
+      func2 = new Funcionario();
+
+      System.Console.WriteLine("Informe o nome e salário da primeira funcionario:");
+      func1.nome = Console.ReadLine();
+      func1.salario = double.Parse(Console.ReadLine());
+
+      System.Console.WriteLine("Informe o nome e salário da segunda funcionario:");
+      func2.nome = Console.ReadLine();
+      func2.salario = double.Parse(Console.ReadLine());
+
+     double media = (func1.salario + func2.salario)/2;
+      System.Console.WriteLine($"Salario médio: {media.ToString("F2")}");
+    }
+  }
+}
