8c3eb9f65a0136367a4176263a8935efa3048dcd
Kissyla <kissylamottapereira@gmail.com>
2022-12-05 15:55:27 -0300

    Exercícico fixação POO pessoa mais velha

---
+++ IntroducaoPOO/PessoaMaisVelha/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PessoaMaisVelha
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Pessoa p1, p2;
+      p1 = new Pessoa();
+      p2 = new Pessoa();
+
+    System.Console.WriteLine("Informe o nome e idade da primeira pessoa:");
+    p1.nome = Console.ReadLine();
+    p1.idade = int.Parse(Console.ReadLine());
+
+      System.Console.WriteLine("Informe o nome e idade da segunda pessoa:");
+      p2.nome = Console.ReadLine();
+      p2.idade = int.Parse(Console.ReadLine());
+
+      if(p1.idade > p2.idade){
+        System.Console.WriteLine($"{p1.nome}, a primeira pessoa informada é mais velha");
+      } else {
+        System.Console.WriteLine($"{p2.nome}, a segunda pessoa informada é mais velha");
+      }
+    }
+  }
+}
---
+++ IntroducaoPOO/readme.md
@@ -0,0 +1,10 @@
+## Exercícios de fixação Introdução Programação Orientada a Objetos 
+
+### Exercício 1 
+
+Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
+velha.
+
+### Exercício 2
+Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+médio dos funcionários.
