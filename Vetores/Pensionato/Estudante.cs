240873c0ddef0da3845ae05635efefe7184b8f82
Kissyla Motta Pereira <kissylamottapereira@gmail.com>
2022-12-11 21:09:51 -0300

    exercicio fixação vetores

---
+++ Vetores/Pensionato/Estudante.cs
@@ -0,0 +1,17 @@
+namespace Pensionato
+{
+  class Estudante {
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public Estudante(string name, string email){
+      Name = name;
+      Email = email;
+    }
+
+    public override string ToString()
+    {
+      return $" {Name}, {Email}";
+    }
+  }
+}
---
+++ Vetores/Pensionato/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pensionato
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    
+
+      Estudante[] vectEstudantes = new Estudante[10];
+
+      System.Console.Write("Quantos quartos serão alugados? ");
+      int qtdQuartos = int.Parse(Console.ReadLine());
+
+      for(int i = 1; i <= qtdQuartos; i++){
+        System.Console.WriteLine($"Aluguel #{i}: ");
+        System.Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        System.Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        System.Console.Write("Quarto: ");
+        int quarto = int.Parse(Console.ReadLine());
+
+        vectEstudantes[quarto] = new Estudante(nome, email);
+
+      }
+
+      System.Console.WriteLine("Quartos ocupados:");
+      for (int i = 0; i < 10; i++)
+      {
+        if(vectEstudantes[i] != null){
+          System.Console.WriteLine($"{i}: {vectEstudantes[i]}");
+
+        }
+
+      }
+
+    }
+  }
+}
